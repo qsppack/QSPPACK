3c1410e3a918a2a78156d96b807c0750ee0adc1d
Yulong Dong <43973097+dongylaq@users.noreply.github.com>
2020-06-05 12:38:35 -0700

    upload

---
+++ Solvers/Optimization model/QSPGetUnitary.m
@@ -0,0 +1,34 @@
+% Get QSP unitary matrix based on given phase vector and point x \in [-1,
+% 1]
+%
+% ----------------------------------------------------------------------
+%
+% Input:
+%       phase, x
+% Output:
+%       targ    -- QSP approximation of target, real(ret(1, 1))
+%
+% ----------------------------------------------------------------------
+%
+% Author:           Yulong Dong, dongyl@berkeley.edu
+% Version:          1.1
+% Last revision:    3/11/2020
+%
+%  ----------------------------------------------------------------------
+
+function [targ] = QSPGetUnitary(phase, x)
+
+Wx = [x, 1j*sqrt(1-x^2); 1j*sqrt(1-x^2), x];
+expphi = exp(1j*phase);
+
+ret = [expphi(1), 0; 0, conj(expphi(1))];
+
+for k = 2:numel(expphi)
+    temp = [expphi(k), 0; 0, conj(expphi(k))];
+    ret = ret * Wx * temp;
+end
+
+targ = real(ret(1,1));
+
+end
+
