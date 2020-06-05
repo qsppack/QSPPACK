ceb5ca55befeafa1320b9c336eb59412bc08232c
Yulong Dong <43973097+dongylaq@users.noreply.github.com>
2020-06-04 20:26:13 -0700

    update

--- Solvers/Optimization model/QSPGrad_sym.m
+++ Solvers/Optimization model/QSPGrad_sym.m
@@ -18,7 +18,7 @@ function [grad, obj] = QSPGrad_sym(phi,delta,opts)
 %
 % Author: Xiang Meng, Yulong Dong
 % Version 1.0 
-% Last revision 2020.2
+% Last revision 02/2020
 %
 %--------------------------------------------------------------------------
 % initial computation
