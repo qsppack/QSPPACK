00c0a6960cb5cbed03ad2f24966b6fc80b856491
JiasuWang <56658795+JiasuWang@users.noreply.github.com>
2022-07-08 16:10:45 -0700

    CM solver and Newton solver
    Uploaded the code for CM solver and Newton solver.

---
+++ Solvers/Optimization/F.m
@@ -0,0 +1,48 @@
+function coe = F(phi, parity, opts)
+%--------------------------------------------------------------------------
+% Compute the Chebyshev coefficients of P_im 
+% P_im: the imagrinary part of the (1,1) element of the QSP unitary matrix.
+
+% Input:
+%          phi --- reduced phase factors 
+%       parity --- Parity of phi (0 -- even, 1 -- odd)
+%
+% Output:
+%            y --- Chebyshev coefficients of P_im w.r.t. 
+%                  T_(2k) -- even, or T_(2k-1) -- odd
+%                  
+%--------------------------------------------------------------------------
+% setup options for CM solver
+if ~isfield(opts,'useReal');              opts.useReal = true; end
+
+
+%%--------------------------------------------------------------------------
+% initial preparation
+d = length(phi);
+dd = 2*d;
+theta = (0:d)'*pi/dd;
+M = zeros(2*dd, 1);
+
+if (opts.useReal == true)
+    f = @(x) QSPGetPim_sym_real(phi, x, parity);
+else
+    f = @(x) QSPGetPim_sym(phi, x, parity);
+end
+
+%-------------------------------------------------------------------------
+% start chebyshev coefficients evaluation
+
+M(1:d+1) = f(cos(theta));
+M(d+2:dd+1)=(-1)^parity * M(d:-1:1);
+M(dd+2:end)= M(dd:-1:2);
+M = fft(M); %FFT w.r.t. columns.
+M = real(M);
+M = M/(2*dd);
+M(2:end-1) = M(2:end-1)*2;
+coe = M(parity+1:2:2*d);
+
+%--------------------------------------------------------------------------
+end
+
+
+
