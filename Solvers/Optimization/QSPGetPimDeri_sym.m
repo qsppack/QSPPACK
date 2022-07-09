ef16dcf950b89b5468a7183c0a8b9b5d95d8f450
JiasuWang <jiasuwang0710@gmail.com>
2022-07-09 13:19:12 -0700

    Update QSPGetPimDeri_sym.m
    This is the version that uses complex arithmetic and run fast. We can compare it with the real version.

--- Solvers/Optimization/QSPGetPimDeri_sym.m
+++ Solvers/Optimization/QSPGetPimDeri_sym.m
@@ -19,26 +19,24 @@ d = length(phi);
 expphi = exp(1j*phi);
 Wx = [x 1j*sqrt(1-x^2); 1j*sqrt(1-x^2) x];
 right = zeros(2,d);
-left = zeros(d,2);
 y = zeros(1,d+1);
 right(:,end) = [expphi(end);0];
 
 for k = d-1:-1:1
     right(:,k) = [expphi(k); conj(expphi(k))] .* Wx * right(:,k+1);
 end
-left(1,:) = transpose(right(:,1));
+left = transpose(right(:,1));
+right = [1j; -1j] .* right;
 
 if (parity==1)
-    left(1,:) = left(1,:) * Wx;
+    left = left * Wx;
 end
+
 for k = 1:d-1
-    left(k+1,:) =  left(k,:) .* [expphi(k) conj(expphi(k))] * Wx;
+    y(k) = 2 * left * right(:, k);
+    left =  left .* [expphi(k) conj(expphi(k))] * Wx;
 end
-
-for k = 1:d
-    y(k) = 2 * left(k,:) .* [1j -1j] * right(:, k);
-end
-
-y(d+1) = left(d,:) * right(:,d);
+y(d) = 2 * left * right(:, d);
+y(d+1) = left * [expphi(end); 0];
 y = imag(y);
 end
